bc298b3e6e9ed9eecea9ac0fef99ce9943a5932f
mmacieja <mmacieja@visa.com>
2026-09-01 14:23:50 +0200

    starting project

---
+++ src/test/java/pl/coderslab/planespotter/PlaneSpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.planespotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneSpotterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
